9c3e1995fb58fa9bbef71284416cd8e58073c163
Kevin Huang <k.d.huang13@berkeley.edu>
2012-05-14 15:04:16 -0700

    blank blog template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog1::Application.config.secret_token = '354bbc75536cdb33a038e1ec0aa68e05ed4c4bc06baf161af4e5c11560c1aa45e9a7783ce9d2b3f6fee35e9e10404f6c92c639fda37fe0173816bdb473565750'
